7026526becc0415760232377b4e3ebbb330e07ca
ta-masahiro <ta.masahiro@gmail.com>
2021-11-03 08:41:41 +0900

    変更中

---
+++ regex_new.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TRUE (1)
+#define FALSE (0)
+// 簡易正規表現エンジン 
+//  https://postd.cc/build-your-own-regex/
+//
+// ? 前の文字と0個か1個一致
+// * 前の文字と0個以上一致
+// . 任意の文字と1個一致
+// ^ 文字の先頭で一致
+// $ 文字の末尾で一致
+// () グループ化
+int match_1(char p, char c) {
+    if ( p == '\0') return TRUE; 
+    if ( c == '\0') return FALSE; 
+    return  p ==  c; 
+}
+int match(char * , char * , int); 
+
+int match_q(char * pattern, char * text, int gr_lvl) {
+    return (match_1( * pattern,  * text) && (match(pattern + 2, text + 1, gr_lvl))) || match(pattern + 2, text, gr_lvl); 
+}
+int match_s(char * pattern, char * text, int gr_lvl) {
+    return (match_1( * pattern,  * text) && (match(pattern, text + 1, gr_lvl))) || match(pattern + 2, text, gr_lvl); 
+}
+char * search_gr_end(char * src, int gr_lvl) {
+    char * p = src;
+    int lvl = gr_lvl;  
+    while ( * p != '\0' ) {
+        if ( * p = '(') {
+            lvl ++ ;
+        } else if (  * p == ')') {
+            if (lvl == gr_lvl) return p; 
+            lvl -- ;
+        }
+        p ++ ; 
+    }
+    return p; 
+}
+int match_g(char * pattern, char * text, int gr_lvl) {
+    // char * group_end = strchr(pattern, ')');
+    char * group_end = search_gr_end(pattern, gr_lvl); 
+    size_t group_size = group_end - pattern;
+    char * group_pattern = (char*)malloc(sizeof(group_size));
+    char * group_text = (char*)malloc(sizeof(group_size));
+    char * remaind_pattern;
+    memcpy(group_pattern, pattern+1, group_size-1);
+    memcpy(group_text, text, group_size);
+    if (*(group_end + 1) =='?') {
+        remaind_pattern = group_end+2;
+        return  (match(group_pattern, group_text, gr_lvl) && match(remaind_pattern, text + group_size, gr_lvl)) ||
+                 match(remaind_pattern,text, gr_lvl);
+    } else if (*(group_end+1) == '*') {
+        remaind_pattern = group_end+2;
+        return  (match(group_pattern, group_text, gr_lvl) && match(pattern, text + group_size, gr_lvl)) ||
+                 match(remaind_pattern,text, gr_lvl);
+    } else {
+        remaind_pattern = group_end + 1;
+        return  match(group_pattern, group_text, gr_lvl) && 
+                 match(remaind_pattern, text + group_size, gr_lvl);
+
+    }
+}
+int match(char * pattern, char * text, int gr_lvl) {
+    if ( * pattern == '\0') return TRUE;
+    else if ( * pattern == '$' &&  * text  == '\0') {
+        return TRUE; 
+    } else if ( * (pattern + 1) == '?') {
+        return match_q(pattern, text, gr_lvl); 
+    } else if ( * (pattern + 1) == '*') {
+        return match_s(pattern, text, gr_lvl); 
+    }else if ( * pattern == '.' &&  * text != '\0' ) {
+        return match(pattern + 1, text + 1, gr_lvl);
+    } else if ( * pattern == '(') {
+        return match_g(pattern, text, gr_lvl + 1);
+    } else return match_1( * pattern,  * text) && match(pattern + 1, text + 1, gr_lvl); 
+}
+int search(char * pattern, char * text) {
+    int i = 0; 
+    if ( * pattern == '^') {
+        return match(pattern + 1, text, 0);
+    } else {
+        while  (* text != '\0') {
+            if (match(pattern, text ++ , 0)) return TRUE;
+        }
+        return FALSE;             
+    }   
+}
+int main(int argc, char * argv[]) {
+    char * pattern = argv[1]; 
+    char * text = argv[2]; 
+     printf("%d\n", search(pattern, text)); 
+    // printf("%d\n", match(pattern, text)); 
+}
